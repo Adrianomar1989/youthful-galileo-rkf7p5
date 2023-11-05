114d4c3b19e69613c950cd037d8f4aabe28c2cbf
CodeSandbox <support@codesandbox.io>
2023-11-05 23:08:13 +0000

    Sandbox snapshot commit

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-var http = require("http");
+var express = require("express");
+const app = express();
+app.use(express.json());
 
-//create a server object:
-http
-  .createServer(function (req, res) {
-    res.write("Hello from CodeSandbox!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+app.get("/", function (req, res) {
+  console.log("Requisicao GET recebida para porta URI /");
+  res.send("Seja bem-vindo à rota principal (/) !");
+});
+
+app.post("/adicionar", function (req, res) {
+  console.log("Requisicao POST recebida para a porta URI /adicionar");
+  res.send("Seja bem-vindo à rota de adicionar (/adicionar) !");
+});
+
+app.put("/atualizar", function (req, res) {
+  console.log("Requisicao PUT recebida para a porta URI /atualizar");
+  res.send("Seja bem-vindo à rota de atualizar (/atualizar) !");
+});
+
+app.delete("deletar", function (req, res) {
+  console.log("Requisicao DELETE rcebida para a porta UIR/ deletar");
+  res.send("Seja bem-vindo à rota de deletar (/deletar)!");
+});
+
+//A porta é uma variável de ambiente
+const porta = process.env.PORT || 8080;
+applisten(porta, () =>
+  console.log("Servidor inicializado na porta: " + porta)
+);
+
+
